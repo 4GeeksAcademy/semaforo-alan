5afdb5eeddef71ff7ae452a2ae786cd3459420ff
Songbird98 <alancabcon@gmail.com>
2025-08-28 20:21:58 +0000

    first commit

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -9,10 +9,10 @@ import "bootstrap"
 import '../styles/index.css'
 
 // components
-import Home from './components/Home';
+import Trafficlight from './components/Trafficlight';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Trafficlight/>
   </React.StrictMode>,
 )
